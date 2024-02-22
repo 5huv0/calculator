f582a5f9cbe46b9044c5137c5dff143a5c961b11
5huv0 <5huv0>
2024-02-23 02:24:01 +0600

    design going

--- script.js
+++ script.js
@@ -1,3 +1,68 @@
-let buttons = document.getElementsByClassName("col");
-let result = document.getElementById("result-cal");
+// let buttons = document.getElementsByClassName("col");
+// let result = document.getElementById("result-cal");
 
+function one(){
+    console.log(1);
+}
+
+
+function two(){
+    console.log(2);
+}
+
+
+function three(){
+    console.log(3);
+}
+
+
+function four(){
+    console.log(4);
+}
+
+
+function five(){
+    console.log(5);
+}
+
+
+function six(){
+    console.log(6);
+}
+
+
+function seven(){
+    console.log(7);
+}
+
+
+function eight(){
+    console.log(8);
+}
+
+
+function nine(){
+    console.log(9);
+}
+
+
+function zero(){
+    console.log(0);
+}
+
+
+function add(){
+    console.log("+");
+}
+
+function susbtract(){
+    console.log("-");
+}
+
+function multiple(){
+    console.log("*");
+}
+
+function divide(){
+    console.log("/");
+}
