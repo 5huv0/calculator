3cbbf6f7ab97b84867b9ba19e37eb80b03c6650e
5huv0 <5huv0>
2024-02-24 01:54:28 +0600

    js logic

--- script.js
+++ script.js
@@ -1,55 +1,51 @@
-// let buttons = document.getElementsByClassName("col");
-// let result = document.getElementById("result-cal");
-
-function one(){
-   let temp = console.log(1);
-   return temp;
-}
+// function one(){
+//   console.log(1); 
+// }
 
 
-function two(){
-    console.log(2);
-}
+// function two(){
+//     console.log(2);
+// }
 
 
-function three(){
-    console.log(3);
-}
+// function three(){
+//     console.log(3);
+// }
 
 
-function four(){
-    console.log(4);
-}
+// function four(){
+//     console.log(4);
+// }
 
 
-function five(){
-    console.log(5);
-}
+// function five(){
+//     console.log(5);
+// }
 
 
-function six(){
-    console.log(6);
-}
+// function six(){
+//     console.log(6);
+// }
 
 
-function seven(){
-    console.log(7);
-}
+// function seven(){
+//     console.log(7);
+// }
 
 
-function eight(){
-    console.log(8);
-}
+// function eight(){
+//     console.log(8);
+// }
 
 
-function nine(){
-    console.log(9);
-}
+// function nine(){
+//     console.log(9);
+// }
 
 
-function zero(){
-    console.log(0);
-}
+// function zero(){
+//     console.log(0);
+// }
 
 
 function add(){
@@ -73,15 +69,72 @@ function dot(){
     console.log(".");
 }
 
-function display(){
-    console.log(temp);
-}
+// function display(){
+    
+// }
 
-function calculate(){
+// function calculate(){
 
-}
+// }
 
 function clearDisplay(){
     console.log(" ");
+}
 
+// function add(){
+
+// }
+
+// function substract(){
+
+// }
+
+// function multiple(){
+
+// }
+
+// function divide(){
+
+// }
+
+// function store(){
+
+// }
+
+function updateInput(number) {
+    document.getElementById("input-display").value += number;
+  }
+
+  function updateInput(value) {
+    
+    let currentInput = document.getElementById("input-display").value;
+  
+    
+    currentInput += value;
+  
+    document.getElementById("input-display").value = currentInput;
+  }
+
+  function add() {
+    let input = document.getElementById("input-display").value;
+    input += "+";
+    document.getElementById("input-display").value = input;
+}
+
+function subtract() {
+    let input = document.getElementById("input-display").value;
+    input += "-";
+    document.getElementById("input-display").value = input;
+}
+
+function multiply() {
+    let input = document.getElementById("input-display").value;
+    input += "x";
+    document.getElementById("input-display").value = input;
+}
+
+function divide() {
+    let input = document.getElementById("input-display").value;
+    input += "/";
+    document.getElementById("input-display").value = input;
 }
