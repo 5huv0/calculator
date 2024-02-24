5e0d5da33849d0357977877b81402e0c6278176c
5huv0 <md88748874@gmail.com>
2024-02-25 00:38:06 +0600

    building logics

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function updateInput(number) {
     currentInput += value;
   
     document.getElementById("input-display").value = currentInput;
-  } // This is to input   numbers multiple time
+  } // This is to input numbers multiple time
 
   function add() {
     let input = document.getElementById("input-display").value;
@@ -40,9 +40,9 @@ function divide() {
 let firstNumber = ""; // Variable to store the first number
 let operation = ""; // Variable to store the operation (+, -, *, /)
 
-function addToInput(value) {
-    document.getElementById("input-display").value += value;
-}
+// function addToInput(value) {
+//     document.getElementById("input-display").value += value;
+// }
 
 function clearInput() {
     document.getElementById("input-display").value = "";
@@ -50,25 +50,25 @@ function clearInput() {
     operation = "";
 }
 
-function add() {
-    firstNumber = document.getElementById("input-display").value;
-    operation = "+";
-    addToInput("+");
-}
+// function add() {
+//     firstNumber = document.getElementById("input-display").value;
+//     operation = "+";
+//     addToInput("+");
+// }
 
-function equals() {
-    let secondNumber = document.getElementById("input-display").value.split(operation)[1];
-    let result;
+// function equals() {
+//     let secondNumber = document.getElementById("input-display").value.split(operation)[1];
+//     let result;
 
-    switch (operation) {
-        case "+":
-            result = parseFloat(firstNumber) + parseFloat(secondNumber);
-            break;
-        // Add cases for other operations if needed
-    }
+//     switch (operation) {
+//         case "+":
+//             result = parseFloat(firstNumber) + parseFloat(secondNumber);
+//             break;
+//         // Add cases for other operations if needed
+//     }
 
-    document.getElementById("input-display").value = result;
-} // This is for adding values 
+//     document.getElementById("input-display").value = result;
+// } // This is for adding values 
 
 
 
